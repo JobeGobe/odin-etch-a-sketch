ceb3ff5a4289a278af92188b72eebd0b58899c72
Jobe <127700499+JobeGobe@users.noreply.github.com>
2023-04-14 20:13:45 -0500

    Implement clear board feature

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const drawContainer = document.querySelector('.draw-container');
+const clearButton = document.querySelector('#clear-button');
 
-let boxWidth = 55;
-let boxHeight = 55;
+let boxWidth = 24;
+let boxHeight = 24;
 
 for(let i = 0; i < (boxWidth * boxHeight); i++){
     const drawBox = document.createElement('div');
@@ -16,4 +17,13 @@ drawContainer.addEventListener('mouseover', (e)=> {
     if(e.target.className === 'draw-pixel'){
         e.target.classList.add('drawn');
     }
+})
+
+clearButton.addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.draw-pixel');
+    console.log(pixels)
+    Array.from(pixels).forEach(element => {
+        element.classList.remove('drawn');
+        console.log(element)
+    });
 })
